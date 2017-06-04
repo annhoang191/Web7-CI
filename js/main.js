5d3b7b7664a072c23f371b42d341b388ccceea25
AnnHoang <hquynhanh19@gmail.com>
2017-06-04 17:21:29 +0700

    added background

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 var Gamefefe = {};
-
 Gamefefe.configs = {
   GAME_WIDTH  : 2046,
   GAME_HEIGHT : 1032,
   MIN_WIDTH   : 800,
   MIN_HEIGHT  : 500,
   MAX_WIDTH   : 2046,
-  MAX_HEIGHT  : 1277
+  MAX_HEIGHT  : 1400
 };
 
 window.onload = function(){
@@ -17,29 +16,31 @@ window.onload = function(){
       update: update
     }, false, false
   );
-
 }
 
 /*==================preparations before game starts==================*/
 var preload = function(){
-  Gamefefe.game.scale.minWidth = 320;
-  Gamefefe.game.scale.minHeight = 480;
-  Gamefefe.game.scale.maxWidth = Gamefefe.configs.GAME_WIDTH;
-  Gamefefe.game.scale.maxHeight = Gamefefe.configs.GAME_HEIGHT;
+  Gamefefe.game.scale.minWidth = Gamefefe.configs.MIN_WIDTH;
+  Gamefefe.game.scale.minHeight = Gamefefe.configs.MIN_HEIGHT;
+  Gamefefe.game.scale.maxWidth = Gamefefe.configs.MAX_WIDTH;
+  Gamefefe.game.scale.maxHeight = Gamefefe.configs.MAX_HEIGHT;
   Gamefefe.game.scale.pageAlignHorizontally = true;
   Gamefefe.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   Gamefefe.game.time.advancedTiming = true;
 
-  //Gamefefe.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
-  Gamefefe.game.load.image('background', 'Assets/background.png');
+  Gamefefe.game.load.image('background', 'Assets/bg.png');
 }
 /*===============================initialize the game==================*/
 var create = function(){
-	Gamefefe.background = Gamefefe.game.add.tileSprite(0,0,Gamefefe.configs.GAME_WIDTH, Gamefefe.configs.GAME_HEIGHT,'background');
+	Gamefefe.game.physics.startSystem(Phaser.Physics.ARCADE);
+  Gamefefe.keyboard = Gamefefe.game.input.keyboard;
+
+  Gamefefe.background  = Gamefefe.game.add.tileSprite(0,0, Gamefefe.configs.GAME_WIDTH, Gamefefe.configs.GAME_HEIGHT, 'background');
+
+  //Create Map
 }
 /*==================Update game state each frame==================*/
 var update = function(){
 
 }
-
