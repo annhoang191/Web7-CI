50f8b11ca3f80294e10d5d22291ffe770e2f6d76
hungphi98 <nguyenhungphi2698@Gmail.com>
2017-06-05 02:19:10 +0700

    Add basic functions for character: animations, turn left right and jump

--- js/PlayerController/PlayerController.js
+++ js/PlayerController/PlayerController.js
@@ -1,6 +1,43 @@
 class PlayerController{
   constructor(x, y, spriteName,configs){
-    this.sprite = Gamefefe.game.add.sprite(x, y, 'Assets', spriteName);
+    this.sprite = Gamefefe.game.add.sprite(x, y,spriteName);
     this.configs = configs;
+    Gamefefe.game.physics.arcade.enable(this.sprite);
+    var walk = this.sprite.animations.add('walk');
+    this.sprite.animations.play('walk',25,true);
+    Gamefefe.game.camera.follow(this.sprite);
+    this.sprite.body.bounce.y = 0.2;
+    this.sprite.body.gravity.y = 500;
+    this.sprite.body.collideWorldBounds = true;
+    this.timeSinceLastJump=0;
+    this.timeSinceLastMove=0
+  }
+
+  update(){
+      this.timeSinceLastJump += Gamefefe.game.time.physicsElapsed;
+      this.timeSinceLastMove+=Gamefefe.game.time.physicsElapsed;
+      if(Gamefefe.keyboard.isDown(this.configs.jump)
+          && this.timeSinceLastJump > 0.4
+        ){
+          this.sprite.y-=80;
+        this.timeSinceLastJump = 0;
+      }
+      if(Gamefefe.keyboard.isDown(this.configs.right)
+          && this.timeSinceLastMove > 0.005
+        ){
+          this.sprite.x+=10;
+        this.timeSinceLastMove = 0;
+      }
+    else if(Gamefefe.keyboard.isDown(this.configs.left)
+        && this.timeSinceLastMove > 0.005
+      ){
+        this.sprite.x-=10;
+      this.timeSinceLastMove = 0;
+    }
+    else{
+         this.sprite.body.velocity.x = 0;
+    }
+
+
   }
 }
--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@ Gamefefe.configs = {
   MIN_WIDTH   : 800,
   MIN_HEIGHT  : 500,
   MAX_WIDTH   : 2046,
-  MAX_HEIGHT  : 700
+  MAX_HEIGHT  : 700,
+  BACKGROUND_SPEED  : 5,
+  PLAYER_CONTROL  : {
+    jump          : Phaser.Keyboard.SPACEBAR,
+    left          : Phaser.Keyboard.LEFT,
+    right         : Phaser.Keyboard.RIGHT
+  }
 };
+
+
 
 window.onload = function(){
   Gamefefe.game = new Phaser.Game(Gamefefe.configs.GAME_WIDTH,Gamefefe.configs.GAME_HEIGHT,Phaser.AUTO,'',
@@ -28,26 +36,42 @@ var preload = function(){
   Gamefefe.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   Gamefefe.game.time.advancedTiming = true;
-
+  //Gamefefe.game.load.image('background', 'Assets/background.png');
   Gamefefe.game.load.image('background', 'Assets/bg.png');
-
   Gamefefe.game.load.tilemap('gamemap', 'Assets/Maps/map0.json', null, Phaser.Tilemap.TILED_JSON);
   Gamefefe.game.load.image('tiles', 'Assets/Tiles/tiles_spritesheet.png');
+  Gamefefe.game.load.atlasJSONHash('player1Walk', 'Assets/Player/p1_walk/p1_walk.png', 'Assets/Player/p1_walk/p1_walk.json');
 }
+
 /*===============================initialize the game==================*/
 var create = function(){
-  Gamefefe.game.physics.startSystem(Phaser.Physics.ARCADE);
-  Gamefefe.keyboard = Gamefefe.game.input.keyboard;
 
-  Gamefefe.background  = Gamefefe.game.add.tileSprite(0,0, Gamefefe.configs.GAME_WIDTH, Gamefefe.configs.GAME_HEIGHT, 'background');
+    //Start the Arcade Physics systems
 
-  //Create Map
-  Gamefefe.map = Gamefefe.game.add.tilemap('gamemap');
-  Gamefefe.map.addTilesetImage('tiles_spritesheet','tiles');
-  Gamefefe.layer = Gamefefe.map.createLayer(0);
-  Gamefefe.layer.resizeWorld();
+    Gamefefe.game.physics.startSystem(Phaser.Physics.ARCADE);
+    Gamefefe.keyboard = Gamefefe.game.input.keyboard;
+
+   Gamefefe.background  = Gamefefe.game.add.tileSprite(0,0, Gamefefe.configs.GAME_WIDTH, Gamefefe.configs.GAME_HEIGHT, 'background');
+
+   //Create Map
+   Gamefefe.map = Gamefefe.game.add.tilemap('gamemap');
+   Gamefefe.map.addTilesetImage('tiles_spritesheet','tiles');
+   Gamefefe.layer = Gamefefe.map.createLayer(0);
+   Gamefefe.layer.resizeWorld();
+
+   Gamefefe.players =[];
+   Gamefefe.players.push(
+       new PlayerController(0,0,'player1Walk',Gamefefe.configs.PLAYER_CONTROL)
+   );
+    Gamefefe.cursors = Gamefefe.game.input.keyboard.createCursorKeys();
+
+
 }
 /*==================Update game state each frame==================*/
 var update = function(){
-  Gamefefe.background.tilePosition.x += 5;
+  //Gamefefe.background.tilePosition.x += 5;
+  for(var player of Gamefefe.players){
+    player.update();
+  }
+  //Gamefefe.game.physics.arcade.collide(player1, Gamefefe.layer);
 }
