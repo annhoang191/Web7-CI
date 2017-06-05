39ff5715df4e117dc3c830d8d11316c1677cbf85
AnnHoang <hquynhanh19@gmail.com>
2017-06-05 15:56:25 +0700

    fix background image bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var Gamefefe = {};
 Gamefefe.configs = {
-  GAME_WIDTH  : 2046,
+  GAME_WIDTH  : 1900,
   GAME_HEIGHT : 700,
   MIN_WIDTH   : 800,
   MIN_HEIGHT  : 500,
+<<<<<<< e20c7151c48686e193d9cec048b6ab28c3177c07
   MAX_WIDTH   : 2046,
   MAX_HEIGHT  : 700,
   BACKGROUND_SPEED  : 5,
@@ -11,6 +12,15 @@ Gamefefe.configs = {
     jump          : Phaser.Keyboard.SPACEBAR,
     left          : Phaser.Keyboard.LEFT,
     right         : Phaser.Keyboard.RIGHT
+=======
+  MAX_WIDTH   : 1900,
+  MAX_HEIGHT  : 700,
+  BACKGROUND_SPEED  : 5,
+  PLAYER_CONTROL    : {
+    jump            : Phaser.Keyboard.SPACEBAR,
+    left            : Phaser.Keyboard.LEFT,
+    right           : Phaser.Keyboard.RIGHT
+>>>>>>> 8db159103be28794e1addab114ca58e7c8f15db1
   }
 };
 
@@ -36,15 +46,25 @@ var preload = function(){
   Gamefefe.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   Gamefefe.game.time.advancedTiming = true;
+<<<<<<< e20c7151c48686e193d9cec048b6ab28c3177c07
   //Gamefefe.game.load.image('background', 'Assets/background.png');
   Gamefefe.game.load.image('background', 'Assets/bg.png');
   Gamefefe.game.load.tilemap('gamemap', 'Assets/Maps/map0.json', null, Phaser.Tilemap.TILED_JSON);
   Gamefefe.game.load.image('tiles', 'Assets/Tiles/tiles_spritesheet.png');
+=======
+
+  //Gamefefe.game.load.image('background', 'Assets/bg.png');
+
+  Gamefefe.game.load.tilemap('gamemap', 'Assets/Maps/map0.json', null, Phaser.Tilemap.TILED_JSON);
+  Gamefefe.game.load.image('tiles', 'Assets/Tiles/tiles_spritesheet.png');
+  //Load sprite
+>>>>>>> 8db159103be28794e1addab114ca58e7c8f15db1
   Gamefefe.game.load.atlasJSONHash('player1Walk', 'Assets/Player/p1_walk/p1_walk.png', 'Assets/Player/p1_walk/p1_walk.json');
 }
 
 /*===============================initialize the game==================*/
 var create = function(){
+<<<<<<< e20c7151c48686e193d9cec048b6ab28c3177c07
 
     //Start the Arcade Physics systems
 
@@ -74,4 +94,28 @@ var update = function(){
     player.update();
   }
   //Gamefefe.game.physics.arcade.collide(player1, Gamefefe.layer);
+=======
+  Gamefefe.game.physics.startSystem(Phaser.Physics.ARCADE);
+  Gamefefe.keyboard = Gamefefe.game.input.keyboard;
+  //Set background color instead of background image to fix bug
+  Gamefefe.game.stage.backgroundColor = '#c6e2ff';
+  //Create Map
+  Gamefefe.map = Gamefefe.game.add.tilemap('gamemap');
+  Gamefefe.map.addTilesetImage('tiles_spritesheet','tiles');
+  Gamefefe.layer = Gamefefe.map.createLayer('groundLayer');
+  Gamefefe.layer.resizeWorld();
+  //Create player
+  Gamefefe.players =[];
+  Gamefefe.players.push(
+    new PlayerController(0,0,'player1Walk',Gamefefe.configs.PLAYER_CONTROL)
+  );
+  Gamefefe.cursors = Gamefefe.game.input.keyboard.createCursorKeys();
+}
+/*==================Update game state each frame==================*/
+var update = function(){
+  for(var player of Gamefefe.players){
+    Gamefefe.game.physics.arcade.collide(Gamefefe.player, Gamefefe.layer);
+    player.update();
+  }
+>>>>>>> 8db159103be28794e1addab114ca58e7c8f15db1
 }
