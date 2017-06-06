35ae3612f0e739185f27f63e308cf548362e961d
AnnHoang <hquynhanh19@gmail.com>
2017-06-06 15:29:49 +0700

    Added map level 1 - loaded jump sound

--- js/main.js
+++ js/main.js
@@ -38,13 +38,17 @@ var preload = function(){
   Gamefefe.game.time.advancedTiming = true;
   //Load audio
   Gamefefe.game.load.audio('theme', ['Assets/Audio/main_theme.mp3', 'Assets/Audio/main_theme.ogg']);
+  Gamefefe.game.load.audio('jumpsound', ['Assets/Audio/jump.mp3', 'Assets/Audio/jump.ogg']);
   /*Cac anh chi em load them sfx nhe, mot file audio nhung co 2 format la mp3 va ogg*/
 
   //Load image and sprite
-  Gamefefe.game.load.tilemap('gamemap', 'Assets/Maps/mapbeta.json', null, Phaser.Tilemap.TILED_JSON);
+  Gamefefe.game.load.tilemap('gamemap', 'Assets/Maps/mapLevel1.json', null, Phaser.Tilemap.TILED_JSON);
   Gamefefe.game.load.image('tiles', 'Assets/Tiles/tiles_spritesheet.png');
   Gamefefe.game.load.image('coins', 'Assets/Items/coinGold.png');
   Gamefefe.game.load.image('spikes', 'Assets/Items/spikes.png');
+  Gamefefe.game.load.image('cloud1', 'Assets/Items/cloud1.png');
+  Gamefefe.game.load.image('cloud2', 'Assets/Items/cloud2.png');
+  Gamefefe.game.load.image('cloud3', 'Assets/Items/cloud3.png');
   Gamefefe.game.load.atlasJSONHash('player1Walk', 'Assets/Player/p1_walk/p1_walk.png', 'Assets/Player/p1_walk/p1_walk.json');
   Gamefefe.game.load.spritesheet('fly', 'Assets/Enemies/flyFly0.png', 74, 34);
 }
@@ -64,6 +68,13 @@ var create = function(){
    Gamefefe.groundLayer = Gamefefe.map.createLayer('groundLayer');
    Gamefefe.map.setCollisionBetween(1, 1000, true, 'groundLayer');
    Gamefefe.groundLayer.resizeWorld();
+   //Add clouds in background
+   Gamefefe.clouds = []
+   Gamefefe.clouds.push(Gamefefe.game.add.sprite(300,100, 'cloud1'));
+   Gamefefe.clouds.push(Gamefefe.game.add.sprite(800,50, 'cloud1'));
+   Gamefefe.clouds.push(Gamefefe.game.add.sprite(1000,150, 'cloud2'));
+   Gamefefe.clouds.push(Gamefefe.game.add.sprite(1500,100, 'cloud3'));
+   Gamefefe.clouds.push(Gamefefe.game.add.sprite(7000,100, 'cloud3'));
    //Display score text
    var text = 'score:';
    Gamefefe.note = Gamefefe.game.add.text(10, 10, '', {
@@ -84,7 +95,7 @@ var create = function(){
        new PlayerController(0,0,'player1Walk',Gamefefe.configs.PLAYER_CONTROL)
    );
     Gamefefe.cursors = Gamefefe.game.input.keyboard.createCursorKeys();
-    Gamefefe.enemies.push(new FlyController(600,350,'fly'));
+    Gamefefe.enemies.push(new FlyController(600,150,'fly'));
 }
 /*==================Update game state each frame==================*/
 var update = function(){
